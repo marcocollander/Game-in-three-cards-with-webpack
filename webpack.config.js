822d15f4ba815333a84a740db5afe9303a4e4e22
Zenon Marek Raubuć <m.raububc@wp.pl>
2022-11-06 05:00:16 +0100

    Update files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,31 @@
+const path = require('path');
+
 module.exports = {
-    entry: {
-        
-    }
-}
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new Html
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  
+  mode: 'development',
+};
