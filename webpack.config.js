6c9686867d8d1b54712a9a504cce6f935acac47f
Zenon Marek Raubuć <m.raububc@wp.pl>
2022-11-06 21:32:38 +0100

    Update files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/js/index.js',
   output: {
@@ -11,21 +11,27 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
-    new Html
+    new htmlWebpackPlugin({
+      hash: true,
+      title: 'Game in three cards',
+      filename: 'index.html',
+      template: './src/templates/index.html',
+    }),
   ],
 
   devServer: {
+    open: true,
     static: {
       directory: path.join(__dirname, 'public'),
     },
     compress: true,
-    port: 9000,
+    port: 5002,
   },
-  
+
   mode: 'development',
 };
